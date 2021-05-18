a9e915a7fba10610b94c1d4f2fb80b608466adbe
Javier Martinez Ribo <javiermribo@gmail.com>
2021-05-18 13:40:22 -0300

    another practise session

--- first-task2.js
+++ first-task2.js
@@ -1,4 +1,4 @@
-let arrayWords = ['mesa', 'avenida', 'computadora', 'alimento', 'bebida', 'intercontinental', 'diccionarios'];
+/*let arrayWords = ['mesa', 'avenida', 'computadora', 'alimento', 'bebida', 'intercontinental', 'diccionarios'];
 
 let wordsLengthValues = [];
 
@@ -27,4 +27,52 @@ function comparatorValues () {
     }
   }
 }
-comparatorValues();
+comparatorValues(); */
+
+
+//EJERCICIO DE MESES
+
+function monthsOfTheYear (month) {
+  for (i = 0; i < month.length; i++) {
+    console.log(month[i] + ` es un gran mes`);
+  }
+}
+
+monthsOfTheYear([
+  `Enero`,
+  `Febrero`,
+  `Marzo`,
+  `Abril`,
+  `Mayo`,
+  `Junio`,
+  `Julio`,
+  `Agosto`,
+  `Septiembre`,
+  `Octubre`,
+  `Noviembre`,
+  `Diciembre`,
+]);
+
+/*let myOddNumbers = [];
+
+  function oddNumber () {
+    for (let i = 0; i <= 12; i++) {
+    if(i % 2 === 1) {
+      myOddNumbers.push(i)
+    }
+  }
+}
+oddNumber();
+
+let monthsOfTheYear = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+function oddMonth (monthsOfTheYear) {
+  for (month in monthsOfTheYear) {
+    if (monthsOfTheYear[i] === myOddNumbers[i]) {
+      console.log(monthsOfTheYear[i] + ` es de los mejores meses`);
+    } else {
+      console.log(monthsOfTheYear[i] + ` es un buen mes`);
+    }
+  }
+}
+oddMonth(monthsOfTheYear);*/
