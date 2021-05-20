1f8a45205a6b8524df3d0d32a3da78feed302e70
Javier Martinez Ribo <javiermribo@gmail.com>
2021-05-20 14:27:50 -0300

    manipulating HTML with JS (DOM)

--- draft.js
+++ draft.js
@@ -1,2 +1,8 @@
+function palindrome () {
+  let userInput = prompt('Ingresa una palabra y descubre si es un palindromo');
+  let invert = userInput.split('').reverse().join('');
+  return invert === userInput;
+}
 
+//console.log('¿Es un palindromo? ' + palindrome());
 
--- first-task2.js
+++ first-task2.js
@@ -1,4 +1,4 @@
-/*let arrayWords = ['mesa', 'avenida', 'computadora', 'alimento', 'bebida', 'intercontinental', 'diccionarios'];
+/* let arrayWords = ['mesa', 'avenida', 'computadora', 'alimento', 'bebida', 'intercontinental', 'diccionarios'];
 
 let wordsLengthValues = [];
 
@@ -29,12 +29,28 @@ function comparatorValues () {
 }
 comparatorValues(); */
 
+/* let arrayWords = [
+  "mesa",
+  "avenida",
+  "computadora",
+  "alimento",
+  "bebida",
+  "intercontinental",
+  "diccionarios",
+];
+
+function findLongestWord (arrayWords) {
+  const longestWord = arrayWords.reduce((longest, currentWord) => {
+    return currentWord.length > longest.length ? currentWord : longest;
+  }, ' ');
+  return longestWord
+}
+
+console.log(findLongestWord(arrayWords)); */
 
 //EJERCICIO DE MESES
-/*Nota para Luis: Hola Luis, como verás logré hacer lo que quería (bien por mi :D). 
-Seguramente el código se puede mejorar. Atte. Javi*/
 
-let theMonthsOfTheYear = [
+/* let theMonthsOfTheYear = [
   `Enero`,
   `Febrero`,
   `Marzo`,
@@ -53,8 +69,19 @@ function monthsOfTheYear(theMonthsOfTheYear) {
   for (let index = 0; index < theMonthsOfTheYear.length; index++) {
     if (index % 2 === 1) {
       console.log(theMonthsOfTheYear[index] + " es un mes par");
-    } else console.log(theMonthsOfTheYear[index] + " es un mes impar");
+    } else {console.log(theMonthsOfTheYear[index] + " es un mes impar")};
   }
 }
 
-monthsOfTheYear(theMonthsOfTheYear);
+monthsOfTheYear(theMonthsOfTheYear); */
+
+const pcontainer = document.querySelector("#pcontainer");
+const newSubtitle = document.createElement("h2");
+newSubtitle.textContent = "Soy un h2 saludando desde el DOM";
+pcontainer.appendChild(newSubtitle);
+
+const newButton = document.createElement("button");
+const referenceGroup = document.querySelector("#pcontainer #p2");
+const pcontainer2 = document.querySelector("#pcontainer");
+newButton.textContent = "Soy un boton creado desde el DOM";
+pcontainer2.insertBefore(newButton, referenceGroup);
