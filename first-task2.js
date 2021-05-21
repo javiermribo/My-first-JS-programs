8118e8eef29d44f1f50fc221d364b15ee90622fd
Javier Martinez Ribo <javiermribo@gmail.com>
2021-05-21 11:35:34 -0300

    minor changes

--- first-task2.js
+++ first-task2.js
@@ -75,6 +75,8 @@ function monthsOfTheYear(theMonthsOfTheYear) {
 
 monthsOfTheYear(theMonthsOfTheYear); */
 
+//PRACTICANDO CON DOM
+
 /* const pcontainer = document.querySelector("#pcontainer");
 const newSubtitle = document.createElement("h2");
 newSubtitle.textContent = "Soy un h2 saludando desde el DOM";
@@ -86,4 +88,38 @@ const pcontainer2 = document.querySelector("#pcontainer");
 newButton.textContent = "Soy un boton creado desde el DOM";
 pcontainer2.insertBefore(newButton, referenceGroup); */
 
+//
 
+function palindrome() {
+  let userInput = prompt("Ingresa una palabra y descubre si es un palindromo");
+  let invert = userInput.split("").reverse().join("");
+  return invert === userInput;
+}
+
+//console.log('¿Es un palindromo? ' + palindrome());
+
+function letsPlay(level2) {
+  let randomNumber = Math.round(Math.random() * 5);
+  let userAttempt = parseInt(
+    prompt("Para pasar al siguiente nivel, adivina el número secreto")
+  );
+  if (userAttempt === randomNumber) {
+    level2();
+  } else {
+    return alert("GAME OVER");
+  }
+}
+
+function level2() {
+  let rightAnswer = "B";
+  let userAttempt = prompt(
+    "No estoy vivo, no tengo huesos pero ando y ando y no me canso ¿Qué soy? A.Auto B.Pantalón C.Pez"
+  ).toUpperCase();
+  if (userAttempt === rightAnswer) {
+    return alert("¡Ganaste!");
+  } else {
+    return alert("GAME OVER");
+  }
+}
+
+//letsPlay(level2);
