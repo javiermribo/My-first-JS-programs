fe79da3320eff1c92b7500f435c9c3cca7962d85
Javier Martinez Ribo <javiermribo@gmail.com>
2021-05-28 21:15:39 -0300

    exercise done

--- appXS.js
+++ appXS.js
@@ -4,3 +4,13 @@ selectingP.innerHTML = selectingP.innerText
   .split(' ')
   .map(word => word.length > 8 ? `<span style="background: yellow"> ${word} </span>` : word)
   .join(' ')
+
+  const selectingP2 = document.querySelector("#pid");
+
+  const selectH1 = document.querySelector('#headId');
+  const referenceItem = document.querySelector('pid');
+  const creatingText = document.createElement('p');
+  creatingText.setAttribute('id', 'wordCounter');
+  creatingText.textContent = `La cantidad de palabras es: ${selectingP2.innerText.length}`;
+
+  headId.insertBefore(creatingText, referenceItem);
